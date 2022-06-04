795951db39dad4564080b6e0f1e58eb485517048
ng-designs <nafis.gimranov@gmail.com>
2022-06-04 08:53:48 +0500

    update: Visual Studio compatibility

---
+++ Backup/AssemblyInfo.cs
@@ -0,0 +1,66 @@
+﻿/*
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''' This is the MIT License, with one addition:
+''' I want attribution in source code form, which means any code of mine you use,
+''' you must keep this entire license intact, including the next line:
+'''
+''' Nik Martin wrote the original version of this software.  
+''' http://www.nikmartin.com
+'''
+'''
+''' Copyright (c) <year> Nik Martin
+'''
+''' Permission is hereby granted, free of charge, to any person obtaining a copy of 
+''' this software and associated documentation files (the "Software"), to deal in 
+''' the Software without restriction, including without limitation the rights to 
+''' use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
+''' the Software, and to permit persons to whom the Software is furnished to do so, 
+''' subject to the following conditions:
+'''
+''' The above copyright notice and this permission notice shall be included in all 
+''' copies or substantial portions of the Software.
+'''
+''' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+''' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+''' FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+''' COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+''' IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+''' CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+*/
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SiUtil")]
+[assembly: AssemblyDescription("Silicon Labs SiUtil dll wrapper for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SiUtil")]
+[assembly: AssemblyCopyright("Copyright ©  2006 Nik Martin")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2b49af77-e124-4d0c-8d61-477696ae6bc0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
